a98df35a39e889d0a4cbf8fbf9cbaf6b546f8c84
SoonYub Hwang <nowonbun@naver.com>
2019-07-02 00:51:55 +0900

    20190702

--- src/main/java/compile/RssManager.java
+++ src/main/java/compile/RssManager.java
@@ -37,8 +37,7 @@ public class RssManager extends AbstractManager {
 		// file:///home/nowonbun/Downloads/rss
 		StringBuffer xml = new StringBuffer();
 		xml.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-		xml.append(
-				"<rss version=\"2.0\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:taxo=\"http://purl.org/rss/1.0/modules/taxonomy/\" xmlns:activity=\"http://activitystrea.ms/spec/1.0/\" >");
+		xml.append("<rss version=\"2.0\" xmlns:rdf=\"http://www.w3.org/1999/02/22-rdf-syntax-ns#\" xmlns:dc=\"http://purl.org/dc/elements/1.1/\" xmlns:taxo=\"http://purl.org/rss/1.0/modules/taxonomy/\" xmlns:activity=\"http://activitystrea.ms/spec/1.0/\" >");
 		xml.append(createTag("channel", () -> {
 			StringBuffer channel = new StringBuffer();
 			channel.append(createTag("title", this.rssTitle));
@@ -55,7 +54,7 @@ public class RssManager extends AbstractManager {
 					StringBuffer item = new StringBuffer();
 					item.append(createTag("title", post.getTitle()));
 					item.append(createTag("link", link));
-					item.append(createTag("description", createDescription(post.getContents())));
+					item.append(createTag("description", "<![CDATA[" + createDescription(post.getContents()) + "]]>"));
 					item.append(createTag("category", getCategoryName(post.getCategory())));
 					item.append(createTag("author", this.rssAuthor));
 					item.append(createTag("guid", link));
