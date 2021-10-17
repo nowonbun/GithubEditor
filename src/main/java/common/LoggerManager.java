cd5aacde59d7e9cd8b468bd14094bdd1b12ff501
nowonbun <nowonbun@naver.com>
2021-10-17 19:09:55 +0900

    Server error

--- src/main/java/common/LoggerManager.java
+++ src/main/java/common/LoggerManager.java
@@ -1,13 +1,13 @@
 package common;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.LoggerContext;
-
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 public class LoggerManager {
 	private static LoggerManager instance = null;
@@ -23,18 +23,19 @@ public class LoggerManager {
 	private LoggerManager() {
 		flyweight = new HashMap<>();
 		File file = new File(LocalPaths.getClassPath() + File.separator + "log4j.xml");
-		LoggerContext context = (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext(false);
-		context.setConfigLocation(file.toURI());
-		//try (InputStream stream = new FileInputStream(file)) {
-		//	PropertyConfigurator.configure(stream);
-		//} catch (Throwable e) {
-		//	throw new RuntimeException(e);
-		//}
+		//LoggerContext context = (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext(false);
+		//context.setConfigLocation(file.toURI());
+		try (InputStream stream = new FileInputStream(file)) {
+			PropertyConfigurator.configure(stream);
+		} catch (Throwable e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	public Logger get(Class<?> clazz) {
 		if (!flyweight.containsKey(clazz)) {
-			flyweight.put(clazz, LogManager.getLogger(clazz));
+			//flyweight.put(clazz, LogManager.getLogger(clazz));
+			flyweight.put(clazz, Logger.getLogger(clazz));
 		}
 		return flyweight.get(clazz);
 	}
